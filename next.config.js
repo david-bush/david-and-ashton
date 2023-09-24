6a1bc2643cc63b4ccc92bb6ca692f24bac75d3b0
david-bush <David.C.Bush@outlook.com>
2023-09-24 16:59:47 -0500

    update image links

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
-    output: 'export',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    basePath: '/ep-static-website'
+    output: 'export'
 }
 
 module.exports = nextConfig

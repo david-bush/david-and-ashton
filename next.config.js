9a32db9680dbde263122c5199f92cd6dde990f31
david-bush <David.C.Bush@outlook.com>
2023-09-24 16:45:22 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
     trailingSlash: true,
     output: 'export',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    basePath: '/ep-static-website'
 }
 
 module.exports = nextConfig
